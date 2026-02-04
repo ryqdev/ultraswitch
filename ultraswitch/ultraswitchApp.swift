cfbc48ee4bcd41bcf330dcf4b79d7b0bd68131ce
ryqdev <ykshang4119@gmail.com>
2026-02-04 19:26:29 +0800

    Initial Commit

---
+++ ultraswitch/ultraswitchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ultraswitchApp.swift
+//  ultraswitch
+//
+//  Created by Ryan Shang on 2026/2/4.
+//
+
+import SwiftUI
+
+@main
+struct ultraswitchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
